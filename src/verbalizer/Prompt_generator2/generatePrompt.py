80ec98e339788529d1865821abe3230f3cd284cc
Julian Sampels <julian.sampels@campus.tu-berlin.de>
2024-02-28 19:26:15 +0100

    workarounds for anatomy special labels

--- src/verbalizer/Prompt_generator2/generatePrompt.py
+++ src/verbalizer/Prompt_generator2/generatePrompt.py
@@ -4,17 +4,20 @@ from verbalisation_module import VerbModule
 
 verb_module = None
 
-def verbalise(tripleList, verbModule):
+def verbalise(tripleList, verbModule, getL):
     ans = "translate Graph to English: "
     #print(tripleList)
     #predicateSet = set([])
     for item in tripleList:
-             ans += f"<H> {item[0]} <R> {item[1]} <T> {item[2]} "
+        try :
+            ans += f"<H> {getL(item[0])} <R> {item[1]} <T> {getL(item[2])} "  
+        except Exception as e:
+            print(item, 'error in generate Prompt, will continue:', repr(e))
 
     return verbModule.verbalise(ans)
 
 
-def verbaliseFile(FILENAME, outputFile):
+def verbaliseFile(FILENAME, outputFile, getL):
     #load module only once
     global verb_module
     if not verb_module:
@@ -29,7 +32,7 @@ def verbaliseFile(FILENAME, outputFile):
     for key, value in tqdm(data.items(), desc="Verbalizing", unit="item"):
         #compute verbalization
         triples = value
-        verbalised_text = verbalise(triples, verb_module)
+        verbalised_text = verbalise(triples, verb_module, getL)
         results.update({key: verbalised_text})
     json_object = json.dumps(results, indent=4)
     with open(outputFile, "w") as outfile:
